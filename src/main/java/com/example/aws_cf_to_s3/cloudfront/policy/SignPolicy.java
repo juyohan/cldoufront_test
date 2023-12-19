37c04e4e1324627161507f209ea529a96f975d16
주요한 <yohan.ju@kt.com>
2023-12-19 17:26:47 +0900

    key 위치 변경

--- src/main/java/com/example/aws_cf_to_s3/cloudfront/policy/SignPolicy.java
+++ src/main/java/com/example/aws_cf_to_s3/cloudfront/policy/SignPolicy.java
@@ -1,10 +1,17 @@
 package com.example.aws_cf_to_s3.cloudfront.policy;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
